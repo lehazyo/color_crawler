cd2561e91a899f1f9789724a1e019b5f423b281c
Алексей Сергиенко <lehazyo@gmail.com>
2018-03-03 23:43:28 +0300

    Изменил настройки алгоритма

--- js/runner.js
+++ js/runner.js
@@ -1,16 +1,15 @@
-app = null;
-
+// в этом файле задаются настройки алгоритма
 (function() {
-  // var app;
+  var app;
   
   var color_crawler_options = {
     width: 800,
     height: 600,
 
-    cycles_to_change_color: 100,
-    color_amplitude: 10,
+    cycles_to_change_color: 10,
+    color_amplitude: 3,
     
-    // grow_type: 1,
+    grow_type: 1,
     // cycles_to_change_grow_type: 100000,
     default_color: [255, 215, 55]
   };
